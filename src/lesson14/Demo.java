d9873767b7a825385aa4daf486cc9f2f33b15f8e
Juli <walarm@meta.ua>
2017-06-23 11:48:18 +0300

    lesson14

--- src/lesson14/Demo.java
+++ src/lesson14/Demo.java
@@ -4,8 +4,15 @@ public class Demo {
         Object objectTest = new Object();
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
+        System.out.println(objectTest.getClass());
+
         objectTest = new Object();
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
+        System.out.println(objectTest.getClass());
+
+        Object objectTest2 = new Object();
+        System.out.println(objectTest2.toString());
+        System.out.println(objectTest2.getClass());
     }
 }
--- src/lesson14/Test.java
+++ src/lesson14/Test.java
@@ -18,15 +18,19 @@ public class Test implements Cloneable {
     }
     public static void main(String[] args) throws Exception{
         Test test = new Test(10000, "name");
+        test.getClass();
         System.out.println(test.toString());
         System.out.println(test.hashCode());
+
         Test test1 = (Test) test.clone();
         System.out.println(test1);
         System.out.println(test1.hashCode());
         System.out.println(test1.toString());
+        test1.getClass();
 
        Test test2 = new Test(test.getSomeNumber(),test.getName());
         System.out.println(test2.toString());
+        test2.getClass();
     }
 
     public int getSomeNumber() {
