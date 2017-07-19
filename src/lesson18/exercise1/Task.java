b7e1a16cdbd8a00d0185778e879b86353544b67d
Juli <walarm@meta.ua>
2017-07-19 20:55:32 +0300

    lesson18

--- src/lesson18/exercise1/Task.java
+++ src/lesson18/exercise1/Task.java
@@ -6,13 +6,33 @@ package lesson18.exercise1;
 public class Task {
     public static void main(String[] args) {
        FileStorage fileStorage = new FileStorage();
+
+        System.out.println("===test1===");
         String[] fileNames = {"test1", "test2"};
         fileStorage.setFiles(fileNames);
+        System.out.println("Start printing name...");
+        printer(fileStorage);
+        System.out.println("Done");
+
+
+        //Test 2 empty files
+        System.out.println("===test2===");
+
+        String[] fileNames2 = {};
+        fileStorage.setFiles(fileNames2);
 
         System.out.println("Start printing name...");
-
         printer(fileStorage);
+        System.out.println("Done");
 
+        //Test 3
+        System.out.println("===test3===");
+
+        String[] fileNames3 = {"test1", "test2", "test3", "test4","test5", "test6"};
+        fileStorage.setFiles(fileNames3);
+
+        System.out.println("Start printing name...");
+        printer(fileStorage);
         System.out.println("Done");
     }
 
